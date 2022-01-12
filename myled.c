f717a6bbdef6c2ce363e898c951f4e06119db632
Soki Adachi <s19C1702SO@s.chibakoudai.jp>
2022-01-12 20:16:48 +0000

    Add RGB unit

--- myled.c
+++ myled.c
@@ -30,17 +30,37 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
         printk(KERN_INFO "receive %c\n",c);
 
 	if(c == '0'){
-		gpio_base[10] = 1 << 23; //LED1を消す
-	}else if(c == '1'){
-		gpio_base[7] = 1 << 23; //LED1をつける
-	}else if(c == '2'){
-		gpio_base[10] = 1 << 24; //LED2を消す
-	}else if(c == '3'){
-		gpio_base[7] = 1 << 24; //LED2をつける
-	}else if(c == '4'){
-		gpio_base[10] = 1 << 25; //
-	}else if(c == '5'){
-		gpio_base[7] = 1 << 25; //
+		gpio_base[10] = 1 << 23; //LED1(赤)を消す
+		gpio_base[10] = 1 << 24; //LED2(緑)を消す
+		gpio_base[10] = 1 << 25; //LED3(青)を消す
+	}else if(c == 'R'){
+		gpio_base[7] = 1 << 23; //LED1()をつける
+		gpio_base[10] = 1 << 24; //LED2()を消す
+		gpio_base[10] = 1 << 25; //LED3()を消す
+	}else if(c == 'G'){
+		gpio_base[10] = 1 << 23; //LED1()を消す
+		gpio_base[7] = 1 << 24; //LED2()をつける
+		gpio_base[10] = 1 << 25; //LED3()を消す
+	}else if(c == 'B'){
+		gpio_base[10] = 1 << 23; //LED1()を消す
+		gpio_base[10] = 1 << 24; //LED2()を消す
+		gpio_base[7] = 1 << 25; //LED3()をつける
+	}else if(c == 'Y'){
+		gpio_base[7] = 1 << 23; //LED1()をつける
+		gpio_base[7] = 1 << 24; //LED2()をつける
+		gpio_base[10] = 1 << 25; //LED3()を消す
+	}else if(c == 'M'){
+		gpio_base[10] = 1 << 23; //LED1()を消す
+		gpio_base[7] = 1 << 24; //LED2()をつける
+		gpio_base[7] = 1 << 25; //LED3()をつける
+	}else if(c == 'P'){
+		gpio_base[7] = 1 << 23; //LED1()をつける
+		gpio_base[10] = 1 << 24; //LED2()を消す
+		gpio_base[7] = 1 << 25; //LED3()をつける
+	}else if(c == 'W'){
+		gpio_base[7] = 1 << 23; //LED1(赤)をつける
+		gpio_base[7] = 1 << 24; //LED2(緑)をつける
+		gpio_base[7] = 1 << 25; //LED3(青)をつける
 	}
 
         return 1; //読み込んだ文字数を返す（この場合はダミーの1）
@@ -87,15 +107,15 @@ static int __init init_mod(void){ //カーネルモジュールの初期化
         
 	gpio_base = ioremap_nocache(0x3f200000, 0xA0); //GPIOのレジスタの最初のアドレス
 	
-	const u32 led1 = 23; //23番にLED1がある
-	const u32 led2 = 24; //24番にLED2がある
-	const u32 led3 = 25; 
+	const u32 led1 = 23; //23番にLED1(赤)がある
+	const u32 led2 = 24; //24番にLED2(緑)がある
+	const u32 led3 = 25; //25番にLED3(青)がある
 	const u32 index1 = led1/10; //GPFSEL2
 	const u32 index2 = led2/10; //GPFSEL2
-	const u32 index3 = led3/10;
+	const u32 index3 = led3/10; //GPFSEL2
 	const u32 shift1 = (led1%10)*3; //9bit
 	const u32 shift2 = (led2%10)*3; //12bit
-	const u32 shift3 = (led3%10)*3; 
+	const u32 shift3 = (led3%10)*3; //15bit
 	const u32 mask1 = ~(0x7 << shift1); //
 	const u32 mask2 = ~(0x7 << shift2); //
 	const u32 mask3 = ~(0x7 << shift3);
